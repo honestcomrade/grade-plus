f9c37452abf69d4f164176cc1a9207504cd31c34
Joe Saraceno <joe@acutulus.co>
2017-04-13 13:34:06 -0700

    Merges old main with test

--- test/test-1.cpp
+++ test/test-1.cpp
@@ -5,7 +5,48 @@
 using namespace std;
 
 int main() {
+
+  // main prompt
+  char buf[100];
+  cout << "Welcome to grade-plus\n";
+  cout << "Import from a file? [Y/N/Q]: ";
+
+  // input loop
+  while (true) {
+    cin >> buf; 
+    if (buf[0] == 'Y' || buf[0] == 'y') {
+      cout << "\nFilename: ";
+      cin >> buf;
+      cout << "Opening file: " << buf << "...\n";
+      break;
+    }
+
+    else if (buf[0] == 'N' || buf[0] == 'n') {
+      cout << "\nCreating new course...\n";
+      break;
+    }
+
+    else if (buf[0] == 'Q' || buf[0] == 'q') {
+      cout << "Goodbye\n";
+      break;
+    }
+
+    else {
+      cout << "Invalid entry\n";
+      continue;
+    }
+  } // opening loop    
+
+  // test instantiation of a student Object.
+  cout << "Testing instantiation of a student Object:\n";
   Student s("1234567", "Jamie", "Green");
 
-  cout << s.getId() << endl;
+  cout << "Name: \t\t" << s.getFirstName() << " " << s.getLastName() << endl;
+  cout << "ID: \t\t" << s.getId() << endl;
+
+  // test writing Student to the database file
+  cout << "Testing writing Student to the database file:\n";
+
+
+
 }
