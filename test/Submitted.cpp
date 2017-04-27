7f4fb2ee8dfdafbb755fcef455a0f26efe736a7b
Jesse Smick <janka102@gmail.com>
2017-04-27 00:28:42 -0700

    Flesh out Submitted class

---
+++ test/Submitted.cpp
@@ -0,0 +1,83 @@
+#include "Submitted.h"
+#include "Database.h"
+
+#include <map>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// Constructor
+Submitted::Submitted(int id, int assignmentId, string studentId,
+                     double pointsEarned, Database db)
+    : id(id), assignmentId(assignmentId), studentId(studentId),
+      pointsEarned(pointsEarned), db(db) {}
+
+// Getters
+int Submitted::getId() const { return id; }
+int Submitted::getAssignmentId() const { return assignmentId; }
+string Submitted::getStudentId() const { return studentId; }
+double Submitted::getPointsEarned() const { return pointsEarned; }
+
+// Setters
+void Submitted::setAssignmentId(int a_assignmentId) {
+  assignmentId = a_assignmentId;
+}
+void Submitted::setStudentId(string a_studentId) { studentId = a_studentId; }
+void Submitted::setPointsEarned(double a_pointsEarned) {
+  pointsEarned = a_pointsEarned;
+}
+
+// Database functions
+void Submitted::update() const {
+  stringstream command;
+  command << "UPDATE submittedAssignments SET ";
+  command << "assignmentId = " << getAssignmentId() << ", ";
+  command << "studentId = '" << getStudentId() << "', ";
+  command << "pointsEarned = " << getPointsEarned() << " ";
+  command << "WHERE id = " << getId() << ";";
+
+  db.execute(command.str());
+}
+void Submitted::remove() const {
+  stringstream command;
+  command << "DELETE FROM submittedAssignments WHERE id = " << getId() << ";";
+
+  db.execute(command.str());
+}
+void Submitted::insert() const {
+  stringstream command;
+  command << "INSERT INTO submittedAssignments VALUES (";
+  command << getAssignmentId() << ", ";
+  command << "'" << getStudentId() << "', ";
+  command << getPointsEarned() << ");";
+
+  db.execute(command.str());
+}
+
+// Static functions
+void Submitted::create(Database db) {
+  string command = "CREATE TABLE IF NOT EXISTS submittedAssignments ("
+                   "assignmentId INTEGER (2) NOT NULL,"
+                   "studentId    TEXT (16)   NOT NULL,"
+                   "pointsEarned NUMERIC (4) NOT NULL"
+                   ");";
+
+  db.execute(command);
+}
+
+map<int, Submitted *> Submitted::read(const Database &db) {
+  map<int, Submitted *> data;
+  vector<vector<string>> results;
+
+  results = db.execute("SELECT * FROM submittedAssignments;");
+
+  for (vector<string> row : results) {
+    int id = stoi(row[0]);
+
+    data[id] = new Submitted(id, stoi(row[1]), row[2], stod(row[3]), db);
+  }
+
+  return data;
+}
--- test/Submitted.h
+++ test/Submitted.h
@@ -3,17 +3,43 @@
 
 #include "Database.h"
 
+#include <map>
 #include <string>
 
 using namespace std;
 
 class Submitted {
- public:
-  Submitted();
-  ~Submitted();
+public:
+  // Constructor
+  Submitted(int, int, string, double, Database db);
 
- private:
+  // Getters
+  int getId() const;
+  int getAssignmentId() const;
+  string getStudentId() const;
+  double getPointsEarned() const;
+
+  // Setters
+  // NOTE: `id` cannot change externally
+  void setAssignmentId(int);
+  void setStudentId(string);
+  void setPointsEarned(double);
+
+  // Database functions
+  void update() const;
+  void remove() const;
+  void insert() const;
+
+  // Static functions
+  static void create(Database);
+  static map<int, Submitted *> read(Database);
+
+private:
   int id;
+  int assignmentId;
+  string studentId;
+  double pointsEarned;
+
   Database db;
 };
 
