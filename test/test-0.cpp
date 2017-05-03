bafab32577afa00cf8f09a689f3fe63ac71c713e
Thomas Munduchira <thomasjojimunduchira@gmail.com>
2017-05-03 15:47:22 -0700

    Remove database file before Course instantiation

--- test/test-0.cpp
+++ test/test-0.cpp
@@ -1,10 +1,17 @@
 #include "Course.h"
 
+#include <cstdio>
 #include <iostream>
 
 using namespace std;
 
 int main() {
+  if (remove("test-0.sqlite") != 0) {
+    cout << "Error with deletion of database file\n";
+  } else {
+    cout << "Database file successfully deleted\n";
+  }
+
   // test instantiation of a Course object.
   cout << "Testing instantiation of a Course object:" << endl;
   Course course("test-0");
