0bca71483ae9ec4a0b795cfee33a72b66f36c688
Joe Saraceno <joe@acutulus.co>
2017-05-06 00:09:57 -0700

    add a prompt function for main in new test file

---
+++ test/funcMapTest.cpp
@@ -0,0 +1,134 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+// these functions will stand in for the execute calls.
+// mock for assignment score
+void f1 (int max) {
+  while (true) {
+    if (0 < max && max <= 100) {
+      cout << "calling function 1 with " << max << " as param\n";
+      break;
+    }
+    else { 
+      cout << max << " is too high for f1.\nTry again: ";
+      cin >> max;
+      continue;
+    }
+  }
+}
+
+void f2 (int max) {
+  while (true) {
+    if (0 < max && max <= 30) {
+      cout << "calling function 2 with " << max << " as param\n";
+      break;
+    }
+    else { 
+      cout << max << " is too high for f2.\nTry again: ";
+      cin >> max;
+      continue;
+    }
+  }
+}
+
+void f3 (int max) {
+  while (true) {
+    if (0 < max && max <= 100) {
+      cout << "calling function 3 with " << max << " as param\n";
+      break;
+    }
+    else { 
+      cout << max << " is too high for f3.\nTry again: ";
+      cin >> max;
+      continue;
+    }
+  }
+}
+
+void f4 (int max) {
+  while (true) {
+    if (0 < max && max <= 100) {
+      cout << "calling function 4 with " << max << " as param\n";
+      break;
+    }
+    else { 
+      cout << max << " is too high for f4.\nTry again: ";
+      cin >> max;
+      continue;
+    }
+  }
+}
+void f5 (int max) {
+  while (true) {
+    if (0 < max && max <= 100) {
+      cout << "calling function 5 with " << max << " as param\n";
+      break;
+    }
+    else { 
+      cout << max << " is too high for f5.\nTry again: ";
+      cin >> max;
+      continue;
+    }
+  }
+}
+
+int prompt(int func) {
+    return func;
+}
+
+int main() {
+
+  int func = 0;
+  int max = 0;
+  char buf[100];
+  // prompt loop
+  while (func >= 0 && func <= 5) {
+    
+    cout << "Which function? [1, 2, 3, 4, 5]\n";
+    cin >> buf;
+    func = atoi(buf);
+    cout << "How many?: ";
+    cin >> buf;
+    max = atoi(buf);
+
+    // sentinel
+    if (prompt(func) < 0) {
+      break;
+    }
+
+    else if (prompt (func) == 1) {
+      f1(max);
+      // break;
+    }
+
+    else if (prompt (func) == 2) {
+      f2(max);
+      // break;
+    }
+
+    else if (prompt (func) == 3) {
+      f3(max);
+      // break;
+    }
+
+    else if (prompt (func) == 4) {
+      f5(max);
+      // break;
+    }
+
+    else if (prompt (func) == 5) {
+      f5(max);
+      // break;
+    }
+
+    else {
+      cout << "invalid\n";
+
+    }
+  }
+}
+
+
