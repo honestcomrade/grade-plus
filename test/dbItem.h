2b6f4af91e80f8396160095dcef2173d3b579a14
Joe Saraceno <joe@acutulus.co>
2017-05-10 16:43:15 -0700

    polish dbItem.h

--- test/dbItem.h
+++ test/dbItem.h
@@ -10,17 +10,6 @@ using namespace std;
 class dbItem {
 
   dbItem(Database &);
-  
-  // Getters
-  // int getId() const;
-  // int getCategoryId() const;
-  // string getName() const;
-  // double getPointsPossible() const;
-
-  // Setters
-  // void setCategoryId(int);
-  // void setName(string);
-  // void setPointsPossible(double);
   virtual void insert() = 0;
   virtual void update() const = 0;
   virtual void remove() const = 0;
