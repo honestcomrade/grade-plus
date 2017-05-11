a968e6fc0f24eb202e50e8b222fdd256d882ad2b
Thomas Munduchira <thomasjojimunduchira@gmail.com>
2017-05-10 19:36:55 -0700

    Add more testing of Course API functions to test-0.cpp

--- test/test-0.cpp
+++ test/test-0.cpp
@@ -16,19 +16,29 @@ int main() {
   // test instantiation of a Course object.
   cout << "Testing instantiation of a Course object:" << endl;
   Course course("test-0");
-  cout << "Created " << course.getName() << endl;
+  cout << "Created " << course.getName() << "\n\n";
 
   // Test students
   course.addStudent("1", "Thomas", "Munduchira");
   course.addStudent("2", "Jesse", "Smick");
   course.addStudent("3", "Joe", "Saraceno");
   course.printStudents();
+  cout << "\n";
+  course.updateStudent("1", "Rahul", "Sharma");
+  course.deleteStudent("2");
+  course.printStudents();
+  cout << "\n";
 
   // Test categories
   course.addCategory("Homework", 20);
   course.addCategory("Labs", 30);
   course.addCategory("Exams", 50);
   course.printCategories();
+  cout << "\n";
+  course.updateCategory(1, "HW", 45);
+  course.deleteCategory(2);
+  course.printCategories();
+  cout << "\n";
 
   // Test assignments
   course.addAssignment(1, "Homework 1", 25);
@@ -38,10 +48,19 @@ int main() {
   course.addAssignment(3, "Exam 1", 100);
   course.addAssignment(3, "Exam 2", 150);
   course.printAssignments();
+  cout << "\n";
+  course.updateAssignment(1, 2, "Labz 1", 30);
+  course.deleteAssignment(2);
+  course.printAssignments();
+  cout << "\n";
 
   // Test submitted
   course.addSubmitted(1, "1", 25);
   course.addSubmitted(3, "2", 10);
   course.addSubmitted(5, "3", 125);
+  course.printSubmitted();
+  cout << "\n";
+  course.updateSubmitted(1, 2, "2", 30);
+  course.deleteSubmitted(2);
   course.printSubmitted();
 }
