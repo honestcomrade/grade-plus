97331b8e317facc43bbe4d20bba688ed54d2036d
Thomas Munduchira <thomasjojimunduchira@gmail.com>
2017-05-18 13:06:52 -0700

    Don't print out full list of submitted if user chooses to filter by student or assignment

--- test/test-1.cpp
+++ test/test-1.cpp
@@ -245,26 +245,28 @@ int main() {
     else if (tolower(input[0]) == 'b') {
       while (true) {
         course.printSubmitted();
+        char option;
         do {
           cout << "\tSELECT student [S]\n\tSELECT assignment [T]\n\tADD "
           "submission [A]\n\tREMOVE submission [R]\n\tMODIFY submission "
           "[M]\n\tGRADE submission [G]\n\tUP [U]\n\tQUIT [Q]\n\t$: ";
           getline(cin, input);
           // filter by student
-          if (tolower(input[0]) == 's') {
+          option = tolower(input[0]);
+          if (option == 's') {
             cout << "Student ID: ";
             getline(cin, input);
             string studentId = input;
             course.printSubmittedOfStudent(studentId);
           }
           // filter by assignment
-          else if (tolower(input[0]) == 't') {
+          else if (option == 't') {
             cout << "Assignment ID: ";
             getline(cin, input);
             int assignmentId = stoi(input);
             course.printSubmittedOfAssignment(assignmentId);
           }
-        } while (tolower(input[0]) == 's' || tolower(input[0]) == 't');
+        } while (option == 's' || option == 't');
         // add submitted
         if (tolower(input[0]) == 'a') {
           cout << "Adding a new submission:\n";
